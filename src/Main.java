d9f2bc85d265a223a6917fd31abaec0cbab96194
DnZ19 <dennis@centerofagile.nl>
2022-12-14 12:50:13 +0100

    some cleanup

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,36 @@ public class Main {
 
         String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
-        //object
+        //object: gets the output of Translator class in a variable (can name this to everything, like banaan)
+        //takes the input of alp and num
 
         Translator banaan = new Translator(alphabetic, numeric);
 
+        //for the while loop and scanner
         boolean play = true;
-        String ongeldig = "ongeldige input";
-
+        String notValid = "not a valid entry";
         Scanner scanner = new Scanner(System.in);
 
 
         while (play) {
-            System.out.println("Type 'x' om te stoppen. Type 'v' om te vertalen");
+            System.out.println("Type 'x' to quit.\nType 'v' for a translation. \nAnswer: ");
             String input = scanner.nextLine().toLowerCase();
 
             if (input.equals("x")) {
                 play = false;
             } else if (input.equals("v")) {
-                System.out.println("type a number between 0-9");
+                System.out.print("type a number between 0 and 9: ");
                 int number = scanner.nextInt();
                 scanner.nextLine();
+
                 if (number < 10) {
                     String result = banaan.translate(number);
-                    System.out.println("De vertaling van: " + number + " is " + result);
+                    System.out.println("The translation of : " + number + " is " + result);
                 } else {
-                    System.out.println(ongeldig);
+                    System.out.println(notValid);
                 }
             } else {
-                System.out.println(ongeldig);
+                System.out.println(notValid);
             }
 
         }
--- src/Translator.java
+++ src/Translator.java
@@ -4,17 +4,21 @@ public class Translator {
 
     HashMap<Integer, String> numericAlpha = new HashMap<>();
 
+    //Constructor: (makes it useable in other classes)
+    // iterates over the length of alp..
+    // and adds for every find a num or alph to the HasMap
+
     public Translator(String[] alphabetic, Integer[] numeric) {
-        for(int i = 0; i < alphabetic.length; i++) {
+        for (int i = 0; i < alphabetic.length; i++) {
             numericAlpha.put(numeric[i], alphabetic[i]);
         }
     }
 
-    public String translate(Integer number){
+    //method
+    public String translate(Integer number) {
         return numericAlpha.get(number);
 
     }
-
 
 
 }
